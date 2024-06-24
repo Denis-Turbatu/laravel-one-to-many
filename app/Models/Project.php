48f6c8ed7cc189660af0edbbdc975b15bece39be
Denis-Turbatu <turbatudenis81@gmail.com>
2024-06-24 14:29:40 +0200

    feat: project() to 'Type' and 'Project' models

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -10,4 +10,9 @@ class Project extends Model
     use HasFactory;
 
     protected $fillable = ["title", "thumb", "description", "start_date", "end_date", "type", "slug"];
+
+    public function projects()
+    {
+        return $this->belongsTo(Type::class);
+    }
 }
